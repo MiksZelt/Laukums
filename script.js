8d9c3b8f3dec8f308c79995756071f92d0da6967
MiksZelt <zeltins.miks24@gmail.com>
2022-02-24 15:00:10 +0200

    2.st

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
-function nolasa(m1,m2,m3){
-    const malas=document.getElementsByClassName("mala");
-     m1=parseFloat(mala1.value);
-     m2=parseFloat(mala2.value);
-     m3=parseFloat(mala3.value);
+function nolasa(m1, m2, m3) {
+    const malas = document.getElementsByClassName("mala");
+    m1 = parseFloat(mala1.value);
+    m2 = parseFloat(mala2.value);
+    m3 = parseFloat(mala3.value);
 
-    if(m1>0 && m2>0 && m3>0){
-        console.log({m1,m2,m3});
-        return{m1,m2,m3};
-    }else{
+    if (m1 > 0 && m2 > 0 && m3 > 0) {
+        console.log({ m1, m2, m3 });
+        return { m1, m2, m3 };
+    } else {
         console.log(false);
         return false;
     }
-    
+
+}
+
+function perimetrs(m1, m2, m3) {
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    const p = m1 + m2 + m3;
+    console.log(p);
+    return (p);
+}
+
+function pusper(m1, m2, m3) {
+    const pu = perimetrs(m1 + m2 + m3) / 2;
+    console.log(pu);
+    return (pu);
+}
+
+function laukums(m1, m2, m3) {
+    const pu = perimetrs(m1 + m2 + m3) / 2;
+    let S = Math.sqrt(pu * ((pu - m1) * (pu - m2) * (pu - m3)));
+    console.log(S);
+    return S;
+}
+
+function irTrij(m1, m2, m3) {
+    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function vienadmal(m1, m2, m3) {
+    const m = nolasa();
+    if (m1 == m2 && m2 == m3) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function vienadsan(m1, m2, m3) {
+    const m = nolasa();
+
 }
